2ac4ca43b9f7942228813673324f121febcffed3
d4man <d4man16@gmail.com>
2022-06-04 12:21:43 +0530

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -1,8 +1,15 @@
 <?php
-    // $hostName = "localhost";
-    // $userName = "root";
-    // $password = "";
-    $databaseName = "propsal_management_system";
+    //  $hostName = "localhost";
+    //  $userName = "root";
+    //  $password = "";
+    // $databaseName = "propsal_management_system";
+
+    //remote db conn.
+     $hostName = "remotemysql.com";
+     $userName = "ADWeeGgUDv";
+     $password = "T279UuKF4Y";
+    $databaseName = "ADWeeGgUDv";
+
     $conn = new mysqli($hostName, $userName, $password, $databaseName);
     if ($conn->connect_error) {
         die("Connection failed due to: " . $conn->connect_error);
