a34e3025365a4d674ade5dc76c07c0c23d3a9a45
dsinghdev <65963433+dsinghdev@users.noreply.github.com>
2022-06-06 15:00:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,4 @@
     // session_destroy();
     header("location:./login/hod_login.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="icon" href=https://i.pinimg.com/originals/ef/3a/76/ef3a766176ca373cdf0f2555f0642c53.jpg">
-</head>
-</html>
+
