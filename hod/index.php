93b2b40ecff50bd739db74c627f9e7c22181eaa5
d4man <d4man16@gmail.com>
2022-06-09 10:33:31 +0530

    Update index.php

--- hod/index.php
+++ hod/index.php
@@ -27,7 +27,7 @@
   <!-- <div>
       <img src="img/elecdocom.png" alt="logo">
     </div> -->
-            <small class="right">Welcome <?php echo $hodNAME?> </small>
+            <b><small class="right">Welcome <?php echo $hodNAME?> </small></b>
         </div>
 
         <nav class='right'><form action="#" method="post"><input type="submit" value="Log Out" name="logout" class="btn btn-outline-danger"></form></nav>
