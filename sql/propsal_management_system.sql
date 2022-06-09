6b2ad123d0f6c45ec5ddf2e26f4aca51613853b2
d4man <d4man16@gmail.com>
2022-06-09 10:27:41 +0530

    Update propsal_management_system.sql

--- sql/propsal_management_system.sql
+++ sql/propsal_management_system.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 29, 2020 at 10:39 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Host: localhost
+-- Generation Time: Jun 09, 2022 at 04:56 AM
+-- Server version: 8.0.13-4
+-- PHP Version: 7.2.24-0ubuntu0.18.04.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `propsal_management_system`
+-- Database: `ADWeeGgUDv`
 --
 
 -- --------------------------------------------------------
@@ -34,14 +35,14 @@ CREATE TABLE `hod_record` (
   `hod_EMAIL` varchar(50) NOT NULL,
   `hod_DEPT` varchar(30) NOT NULL,
   `hod_PASSWORD` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `hod_record`
 --
 
 INSERT INTO `hod_record` (`hod_ID`, `hod_FIRST_NAME`, `hod_LAST_NAME`, `hod_EMAIL`, `hod_DEPT`, `hod_PASSWORD`) VALUES
-(1, 'Dinesh', 'Gupta', 'dinesh@hod.com', 'Computer Science', '1234');
+(2, 'Dinesh', 'Gupta', 'dgupta@hod.com', 'CSE', '1234');
 
 -- --------------------------------------------------------
 
@@ -54,8 +55,8 @@ CREATE TABLE `project_notification` (
   `project_id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
   `hod_id` int(11) NOT NULL,
-  `seen` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `seen` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `project_notification`
@@ -68,7 +69,13 @@ INSERT INTO `project_notification` (`notification_id`, `project_id`, `student_id
 (10, 1, 1, 1, 1),
 (11, 3, 1, 1, 1),
 (12, 5, 1, 1, 1),
-(13, 3, 1, 1, 0);
+(13, 3, 1, 1, 0),
+(14, 5, 1, 1, 0),
+(15, 5, 1, 1, 0),
+(16, 5, 1, 1, 0),
+(17, 5, 1, 1, 0),
+(18, 5, 1, 1, 1),
+(19, 5, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -86,16 +93,17 @@ CREATE TABLE `project_record` (
   `project_BATCH` int(11) NOT NULL,
   `project_COURSE` varchar(50) NOT NULL,
   `project_COMMENT` varchar(500) DEFAULT NULL,
-  `project_STATUS` tinyint(1) NOT NULL DEFAULT 0,
+  `project_STATUS` tinyint(1) NOT NULL DEFAULT '0',
   `project_file` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `project_record`
 --
 
 INSERT INTO `project_record` (`project_ID`, `student_ID`, `hod_ID`, `project_TITLE`, `project_date`, `project_PROFESSOR`, `project_BATCH`, `project_COURSE`, `project_COMMENT`, `project_STATUS`, `project_file`) VALUES
-(5, 1, 1, 'Online Project Proposal', 11-11-2020, ' ', 3, 'Computer Science', ' ', 2, '../server/uploads/5ef73832382d16.62240900.pdf');
+(5, 1, 1, 'Online Project Proposal', '2009-06-20', ' ', 3, 'Computer Science', ' ', 1, '../server/uploads/5ef73832382d16.62240900.pdf'),
+(6, 1, 1, 'Technical fest ', '2022-06-02', NULL, 6, 'Computer Science ', NULL, 0, '../server/uploads/629ef1905d37a1.36791273.txt');
 
 -- --------------------------------------------------------
 
@@ -109,7 +117,7 @@ CREATE TABLE `student_hod_chat` (
   `hod_id` int(11) NOT NULL,
   `message_body` varchar(400) NOT NULL,
   `message_sender` varchar(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `student_hod_chat`
@@ -136,16 +144,16 @@ CREATE TABLE `student_record` (
   `student_FIRST_NAME` varchar(50) NOT NULL,
   `student_LAST_NAME` varchar(50) NOT NULL,
   `student_PASSWORD` varchar(50) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `student_record`
 --
 
 INSERT INTO `student_record` (`student_ID`, `student_EMAIL`, `student_FIRST_NAME`, `student_LAST_NAME`, `student_PASSWORD`, `status`) VALUES
-(1, 'Rahul@gmail.com', 'Damanjot Singh', 'Jibran', '123456', 1),
-(2, 'umairjibran@gmail.com', 'Umair', 'Jibran', '123456', 1);
+(1, 'daman42a@gmail.com', 'Damanjot', 'Singh', '12345', 1),
+(2, 'Rahulpreet936@gmail.com', 'Rahulpreet', 'Singh', '12345', 1);
 
 --
 -- Indexes for dumped tables
@@ -191,19 +199,19 @@ ALTER TABLE `student_record`
 -- AUTO_INCREMENT for table `hod_record`
 --
 ALTER TABLE `hod_record`
-  MODIFY `hod_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `hod_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `project_notification`
 --
 ALTER TABLE `project_notification`
-  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `project_record`
 --
 ALTER TABLE `project_record`
-  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `student_hod_chat`
@@ -215,7 +223,7 @@ ALTER TABLE `student_hod_chat`
 -- AUTO_INCREMENT for table `student_record`
 --
 ALTER TABLE `student_record`
-  MODIFY `student_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `student_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
