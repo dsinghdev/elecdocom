0d240a7da2e6fe47192aff5605961e5136910b8b
d4man <d4man16@gmail.com>
2022-06-13 23:07:18 +0530

    Update index.php

--- hod/index.php
+++ hod/index.php
@@ -32,7 +32,7 @@
 
         <nav class='right'><form action="#" method="post"><input type="submit" value="Log Out" name="logout" class="btn btn-outline-danger"></form></nav>
         <br><br><br>
-        <h4><caption class='center'>Approve Students</caption></h4><br>
+        <h4><caption style="color: brown;" class='center'>Approve Students</caption></h4><br>
         <table class="table">
    
             <tr>
